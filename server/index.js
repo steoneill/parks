4489ba72382f6268144cdd5b3948d461fe2c35d8
Ste <s.o'neill@brassagency.com>
2018-12-17 17:00:14 +0000

    added graphql

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require("express"),
+  graphqlHTTP = require("express-graphql"),
+  { buildSchema } = require("graphql"),
   app = express();
 
-app.get("/", (req, res) => {
-  res.send("working");
-});
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+let root = {
+  hello: () => {
+    return "Hello World!";
+  }
+};
+app.use(
+  "/",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
 
 app.listen("8000", () => {
   console.log("server starting on 8000");
