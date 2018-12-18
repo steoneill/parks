6000d53d0634fd090ffc889823db9af44e834648
Ste <ste.tpe@gmail.com>
2018-12-18 17:27:01 +0000

    wip for adding theme parks to schema

--- server/index.js
+++ server/index.js
@@ -1,23 +1,52 @@
-const {
-  GraphQLServer
-} = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga");
+
+const Themeparks = require("themeparks");
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    park(name: String): [String]!
+    allParks: [String],
   }
-`
+
+
+
+`;
 
 const resolvers = {
   Query: {
-    hello: (_, {
-      name
-    }) => `Hello ${name || 'World'}`,
-  },
-}
+    park: (_, { name }) => {
+      let Rides = [];
+
+      let park = new Themeparks.Parks[name]();
+      park
+        .GetWaitTimes()
+        .then(function(rides) {
+          // print each wait time
+          for (var i = 0, ride; (ride = rides[i++]); ) {
+            Rides.push(ride);
+          }
+        }, console.error)
+        .then(() => {
+          console.log(Rides);
+          return Rides;
+        });
+    },
+    allParks: () => {
+      let parks = [];
+      for (var park in Themeparks.Parks) {
+        console.log(park);
+        if (park.indexOf("Disney") != -1) {
+          parks.push(new Themeparks.Parks[park]().Name);
+        }
+      }
+      return parks;
+    }
+  }
+};
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers
-})
-server.start(() => console.log('Server is running'))
+});
+server.start(() => console.log("Server is running"));
