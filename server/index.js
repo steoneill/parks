ff9d8b2a1f24b7bd96047bdb5839db403e29f07c
Ste <ste.tpe@gmail.com>
2018-12-18 11:49:41 +0000

    changed to yoga

--- server/index.js
+++ server/index.js
@@ -1,33 +1,23 @@
-const express = require("express"),
-  graphqlHTTP = require("express-graphql"),
-  { buildSchema } = require("graphql"),
-  app = express();
+const {
+  GraphQLServer
+} = require('graphql-yoga')
 
-let schema = buildSchema(`
+const typeDefs = `
   type Query {
-    hello: String
+    hello(name: String): String!
   }
-`);
+`
 
-let root = {
-  hello: () => {
-    return "Hello World!";
-  }
-};
+const resolvers = {
+  Query: {
+    hello: (_, {
+      name
+    }) => `Hello ${name || 'World'}`,
+  },
+}
 
-app.get("/", (req, res) => {
-  res.send("rest api endpoint");
-});
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true
-  })
-);
-
-app.listen("8000", () => {
-  console.log("server starting on 8000");
-});
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+server.start(() => console.log('Server is running on localhost:4000'))
